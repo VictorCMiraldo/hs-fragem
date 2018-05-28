270d98d4601f2d04515e4342266015ec7bf4e55a
Victor Miraldo <v.cacciarimiraldo@gmail.com>
2018-05-28 17:59:35 +0200

    Got interpolation working; needs code cleanup

--- src/Fragem/Syntax.hs
+++ src/Fragem/Syntax.hs
@@ -32,11 +32,6 @@ data Duration
   | SixtyFourth
 -}
 
--- |Returns the reciprocal of the relative value of a duration,
---  that is, @durationValue Quarter = 4@.
-durationRecip :: Duration -> Float
-durationRecip (Dot d)    = 2/3 * durationRecip d
-durationRecip (Simple i) = 2 ** (fromIntegral i)
 {-
 durationRecip Maxima      = 0.125
 durationRecip Longa       = 0.25
@@ -52,7 +47,16 @@ durationRecip SixtyFourth = 64
 
 -- |Returns the fraction of the measure used by the duration.
 durationValue :: Duration -> Float
-durationValue = (1/) . durationRecip
+durationValue (Simple i) = 1 / (2 ** (fromIntegral i))
+durationValue (Dot d)
+  = let nd  = countDots d + 1
+        nd' = 2 ** nd
+     in durationValue (noDots d) * (1 + ((nd' - 1) / nd'))
+  where countDots (Dot d) = 1 + countDots d
+        countDots _       = 0
+        noDots (Dot d)    = noDots d
+        noDots x          = x
+                        
 
 -- |Interpolates a float in a duration. We play a game close
 --  to Newton's method here.
@@ -66,17 +70,21 @@ dist :: Float -> Float -> Float
 dist x y = abs (x - y)
 
 down :: Duration -> Duration
-down (Dot (Simple i))             = (Simple i) 
+{- down (Dot (Simple i))             = (Simple i) 
 down (Dot (Dot (Simple i)))       = (Dot (Simple i)) 
 down (Dot (Dot (Dot (Simple i)))) = (Dot (Dot (Simple i))) 
 down (Simple i)                   = Dot (Dot (Dot (Simple (i+1)))) 
+-}
+down (Simple i) = Dot (Simple (i + 1))
+down (Dot x)    = x
 
 go :: Float -> Duration -> Float -> Duration
-go f cand d
-  = let d' = dist (durationValue (down cand)) f
+go tgt cand d
+  = let cand' = down cand
+        d' = dist (durationValue cand') tgt
      in if d' < d 
-        then go f (down cand) d'
-        else cand   
+        then go tgt cand' d'
+        else cand
 
 -- |A 'NotePitch' can be a rest or a pitch.
 data NotePitch
