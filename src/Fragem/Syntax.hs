44130e4e17fe35680e74cf900a83919e2d37d53d
Victor Miraldo <v.cacciarimiraldo@gmail.com>
2018-06-11 16:43:00 +0200

    Many changes; adhered to the latest format

--- src/Fragem/Syntax.hs
+++ src/Fragem/Syntax.hs
@@ -1,71 +1,23 @@
 module Fragem.Syntax where
 
--- |A 'Duration' indicates the relative duration of a note.
---  Each dot increases the duration 
-data Duration
-  -- |A @Simple i@ lasts for @1/(2 ** i)@ of a measure.
-  = Simple Int 
-  -- |A @Dot d@ lasts for @d + .5*d@ of a measure.
-  | Dot Duration
-  -- |A @Irregular n d@ lasts for
-  | Irregular Int Duration
-  deriving (Eq , Show , Ord)
-
--- |Returns the fraction of the measure used by the duration.
-durationValue :: Duration -> Float
-durationValue (Simple i) = 1 / (2 ** (fromIntegral i))
-durationValue (Irregular n d)
-  = durationValue d * (fromIntegral n - 1) / fromIntegral n
-durationValue (Dot d)
-  = let nd  = countDots d + 1
-        nd' = 2 ** nd
-     in durationValue (noDots d) * (1 + ((nd' - 1) / nd'))
-  where countDots (Dot d) = 1 + countDots d
-        countDots _       = 0
-        noDots (Dot d)    = noDots d
-        noDots x          = x
-                        
-
--- |Interpolates a float in a duration. We play a game close
---  to Newton's method here.
---
---  TODO: Add how many dots should we consider; this changes
---        everything.
-durationInterp :: Float -> Duration
-durationInterp f
-  = let start = Simple (-2)
-     in go f start (dist (durationValue start) f)
-  where
-    dist :: Float -> Float -> Float
-    dist x y = abs (x - y)
-
-    down :: Duration -> Duration
-    down (Simple i) = Dot (Simple (i + 1))
-    down (Dot x)    = x
-
-    go :: Float -> Duration -> Float -> Duration
-    go tgt cand d
-      = let cand' = down cand
-            d' = dist (durationValue cand') tgt
-         in if d' < d 
-            then go tgt cand' d'
-            else cand
-
--- |A 'NotePitch' can be a rest or a pitch.
-data NotePitch
-  = Rest
-  | Pitch Int
-  deriving (Eq , Show , Ord)
+type Offset = Int
+type Ticks  = Int
 
 -- |A 'Note' has a duration, a pitch and it might be tied
 --  to the note that comes after.
 data Note = Note
+  -- | Absolute offset of note from start of the piece; in ticks
+  { noteDelay    :: Offset
   -- | Duration of a note
-  { noteDuration :: Duration
+  , noteDuration :: Ticks
   -- | Pitch of the note (or rest)
-  , notePitch    :: NotePitch
-  -- | Whether this note is tied to the next one or not.
-  , noteTie      :: Bool
+  , notePitch    :: Int
+  } deriving (Eq , Show , Ord)
+
+-- |Musical time signature.
+data TimeSig = TimeSig
+  { timesigNum   :: Int
+  , timesigDenum :: Int
   } deriving (Eq , Show , Ord)
 
 -- |A 'Measure' consists in a list of notes.
@@ -74,24 +26,14 @@ newtype Measure = Measure { measureNotes :: [Note] }
 
 -- |A 'Section' consists in a a list of measures in a given time signature.
 data Section = Section
+  -- |Current section's time signature
   { sectionSignature :: TimeSig
-  , sectionMeasires  :: [Measure]
+  -- |Current section's ticks-per-beat
+  , sectionTPB       :: Int
+  -- |Measures in the given tempo
+  , sectionMeasures  :: [Measure]
   } deriving (Eq , Show , Ord)
 
--- |Musical time signature.
-data TimeSig = TimeSig Int Int
+-- |A 'Voice' is a list of sections.
+newtype Voice = Voice { pieceSections :: [Section] }
   deriving (Eq , Show , Ord)
-
--------------------------
--- * Some Processing * --
--------------------------
-
--- VCM: TODO: Refactor this somewhere else
-
-(.+) :: Duration -> Duration -> Duration
-p .+ q = durationInterp (durationValue p + durationValue q)
-
--- |@interpolateBy n ms@ will return a list of pitches as
---  seen at every @n@th beat.
-interpolateBy :: Duration -> [Measure] -> [[Int]]
-interpolateBy = undefined
--- src/Fragem/Syntax/FromMidi.hs
+++ src/Fragem/Syntax/FromMidi.hs
@@ -1,11 +1,14 @@
+{-# LANGUAGE RankNTypes #-}
 module Fragem.Syntax.FromMidi (fromMidi) where
 
+import Control.Arrow ((***), (&&&))
 import Control.Monad
+import Control.Monad.State
 import Data.Function (on)
 import Data.List (sortBy, groupBy)
 
-import qualified Codec.Midi as Midi
-import qualified Data.Map   as M
+import qualified Codec.Midi      as Midi
+import qualified Data.Map.Strict as M
 
 import           Fragem.Syntax
 
@@ -30,34 +33,71 @@ isNoteEvent (Midi.NoteOn  _ _ _) = True
 isNoteEvent (Midi.NoteOff _ _ _) = True
 isNoteEvent _                    = False
 
--- |Returns the first time signature it finds in a track.
---
--- TODO: how to support different time signatures?
-getTimeSig :: [Midi.Track a] -> TimeSig
-getTimeSig = maybe (error "getTimeSig: no time signature!") id
-           . head
-           . filter (maybe False (const True))
-           . map getTimeSig1
-  where
-    getTimeSig1 :: Midi.Track a -> Maybe TimeSig
-    getTimeSig1 tr = case filter (isTimeSig . snd) tr of
-                       ((_ , Midi.TimeSignature n d _ _) : _)
-                          -> Just $ TimeSig n (2^d)
-                       _ -> Nothing
 
-type Ticks = Midi.Ticks
-type Key   = Midi.Key
+--------------------------------
+-- * Processing a Midi File * --
+--------------------------------
+--
+-- $stepone
+--
+-- The first step is to make everything in terms of absolute offsets,
+-- this makes thinks much simpler. Even before that, we better make
+-- sure that this is a 'Midi.MultiTrack' file, that is, each track
+-- is separate from each other. We leave the processing of other 'Midi.FileType's
+-- as future work.
+--
+-- Making the deltas in the midi file into absolute delays is simple.
+-- We do so by traversing every track and adding up all the offsets.
+-- The function 'midiAbsoluteOffsets' performs the conversion.
+--
+
+-- | Returns whether a Midi file is a multi-track file. (Type 1)
+midiIsMultiTrack :: Midi.Midi -> Bool
+midiIsMultiTrack = (Midi.MultiTrack ==) . Midi.fileType
+
+-- | Returns whether a Midi file has a 'TicksPerBeat' tempo indication.
+--   We do not support ticks-per-second yet. TODO
+--
+--   Found a description of that here:
+--     https://www.music.mcgill.ca/~ich/classes/mumt306/StandardMIDIfileformat.html#BM2_1
+midiIsTicksPerBeat :: Midi.Midi -> Bool
+midiIsTicksPerBeat = isTPB . Midi.timeDiv
+  where
+    isTPB (Midi.TicksPerBeat _) = True
+    isTPB _                     = False
+
+-- | Makes all the track's delays into absolute offsets.
+--   IMPORTANT: this only works for multi-track files. See 'midiIsMultiTrack'.
+midiAbsoluteOffsets :: Midi.Midi -> Midi.Midi
+midiAbsoluteOffsets (Midi.Midi ft td tracks)
+  = Midi.Midi ft td (map (tracksAbsoluteOffsets 0) tracks)
+  where
+    tracksAbsoluteOffsets :: Midi.Ticks -> Midi.Track Midi.Ticks -> Midi.Track Midi.Ticks
+    tracksAbsoluteOffsets acu [] = []
+    tracksAbsoluteOffsets acu ((t , msg):rest)
+      = (t + acu , msg) : tracksAbsoluteOffsets (t + acu) rest
+
+-- $steptwo
+--
+-- Now, we have to gather the TimeSig messages and divide the tracks into chunks
+-- in the same time signatures. We call these 'Sections'. This requires much
+-- more processing. We need to first mine the tracks for TimeSig messages; record
+-- their absolute offset on a interval map and split the sections.
+--
 
 -- |A 'NoteEvent' consists in some time information,
 --  some duration in ticks, and a pitch.
-type NoteEvent = (Ticks , Ticks , Key)
+type NoteEvent = (Midi.Ticks , Midi.Ticks , Midi.Key)
 
--- |Gets the note events.
---  the note is on or off. True indicates on.
-getNoteEvents :: Midi.Track Midi.Ticks -> [NoteEvent]
-getNoteEvents = sortBy go . process M.empty 0 . simplify
+offset :: NoteEvent -> Midi.Ticks
+offset (o , _ , _) = o
+
+-- |Get all the note events in a track; convert them to an easier to handle format.
+--  PRECONDITION: the track must be in absolute-time-reference format.
+midiGetNoteEvents :: Midi.Track Midi.Ticks -> [NoteEvent]
+midiGetNoteEvents = sortBy go . process M.empty . simplify
   where
-    go = compare `on` (\(x , y , z) -> x)
+    go = compare `on` offset
     
     -- Processing the stream of notes is simple. Everytime we see
     -- a NoteOn event we add it on a map and record when it was "on".
@@ -66,21 +106,21 @@ getNoteEvents = sortBy go . process M.empty 0 . simplify
     --
     -- TODO: We can identify ties by noticing that the duration of
     --       a note spans accross a measure bound.
-    process :: M.Map Key Ticks
-            -> Ticks
+    process :: M.Map Midi.Key Ticks
             -> [(Midi.Ticks , Bool , Int)]
             -> [NoteEvent]
-    process m t []
+    process m []
       | M.null m  = []
       | otherwise = error "getNoteEvents: unclosed notes"
-    process m t ((delay , isOn , key):ks)
-      = let t' = t + delay
-         in if isOn
-            then process (M.insert key t' m) t' ks
-            else case M.lookup key m of
-                      Just t0 -> (t0 , t' - t0 , key)
-                               : process (M.delete key m) t' ks
-                      Nothing -> error "getNoteEvents: NoteOff without corresponding NoteOn"
+    process m ((offset , isOn , key):ks)
+      | isOn      = process (M.insert key offset m) ks
+      | otherwise = case M.lookup key m of
+                      Just t0 -> (t0 , offset - t0 , key)
+                               : process (M.delete key m) ks
+                               -- VCM: Some of the Well Temepred Clavier have NoteOffs without
+                               --      corresponding noteOns...
+                      Nothing -> -- error "getNoteEvents: NoteOff without corresponding NoteOn"
+                               process m ks
     
     -- returns a list of the noteon and noteoff events in a midi track.
     -- the boolean flag indicates a noteon.
@@ -89,42 +129,104 @@ getNoteEvents = sortBy go . process M.empty 0 . simplify
       where
         convert (x , Midi.NoteOn  _ key _) = (x , True  , key)
         convert (x , Midi.NoteOff _ key _) = (x , False , key)
+ 
+-- VCM: TODO: We will not record tempo changes here; only time signature
+--            changes; it should not be too hard to implement tempo changes
+--            later.
+
+data Step2State = Step2State
+  { ticksPerBeat   :: Midi.Ticks
+  , timesigChanges :: M.Map Midi.Ticks TimeSig
+  } deriving (Eq , Show)
+
+type Step2 = State Step2State
+
+-- |Get the time signatures of a track and organize them in a map.
+midiGetTimeSigs :: Midi.Track Midi.Ticks -> M.Map Midi.Ticks TimeSig
+midiGetTimeSigs = M.map convert . M.fromList . filter (isTimeSig . snd)
+  where
+    convert :: Midi.Message -> TimeSig
+    convert (Midi.TimeSignature n d _ _) = TimeSig n (2^d)
+    convert _ = error "midiGetTimeSigs: not a time signature!! Did you forget to filter?"
+
+-- |Given a Midi file, returns the state we need
+--  to process it into sections.
+step2PrepareState :: Midi.Midi -> Step2State
+step2PrepareState (Midi.Midi _ (Midi.TicksPerBeat tpb) tr)
+  = Step2State tpb (M.unions $ map midiGetTimeSigs tr)
+
+-- |Returns the time signature at a certain tick.
+step2TimeSigFor :: Midi.Ticks -> Step2 TimeSig
+step2TimeSigFor t
+  = (maybe err snd . M.lookupLE t) <$> (timesigChanges <$> get)
+  where
+    err = error "step2TimeSigFor: No time signature found"
+
+-- |Was there a time signature in between the ticks? This
+--  happens when the time signature of two ticks is different.
+step2TimeSigChange :: Midi.Ticks -> Midi.Ticks -> Step2 Bool
+step2TimeSigChange t t'
+  = (/=) <$> step2TimeSigFor t <*> step2TimeSigFor t'
+
+step2TicksPerMeasure :: Midi.Ticks -> Step2 Midi.Ticks
+step2TicksPerMeasure t
+  = do (TimeSig n _) <- step2TimeSigFor t
+       tpb           <- ticksPerBeat <$> get
+       return $ n * tpb
+
+step2MeasureChange :: Midi.Ticks -> Midi.Ticks -> Step2 Bool
+step2MeasureChange t t'
+  = do tsigChange <- step2TimeSigChange t t'
+       if tsigChange
+       then return True
+       else do tpm <- step2TicksPerMeasure t -- its the same for t' since timesig did NOT change
+               return (t `div` tpm /= t' `div` tpm)
+
+groupByM :: (Monad m) => (a -> a -> m Bool) -> [a] -> m [[a]]
+groupByM cond []     = return []
+groupByM cond (x:xs) = go [x] xs
+  where
+    go acu     []
+      = return [reverse acu]
+    go (a:acu) (x:xs)
+      = do flag <- cond a x 
+           if flag
+           then go (x:a:acu) xs
+           else (reverse (a:acu) :) <$> (go [x] xs)
+
+-- |Return the sections of a midi file. The outer list has the same
+--  length as the number of tracks in the midi file.
+midiGetSections :: Midi.Midi -> [[Section]]
+midiGetSections midi = evalState (go $ Midi.tracks midi) (step2PrepareState midi)
+  where
+    go :: [Midi.Track Midi.Ticks] -> Step2 [[Section]]
+    go = mapM (\tr -> splitTS (midiGetNoteEvents tr) >>= mapM splitMS)
+
+    -- Group by time signature block
+    splitTS :: [NoteEvent] -> Step2 [[NoteEvent]]
+    splitTS = groupByM (\(t0 , _ , _) (t1 , _ , _) -> not <$> step2TimeSigChange t0 t1)
+
+    -- Group by measure
+    splitMS :: [NoteEvent] -> Step2 Section
+    splitMS []     = error "Can't split an empty section"
+    splitMS (x:xs)
+      = do tpb <- ticksPerBeat <$> get
+           ts  <- step2TimeSigFor (offset x)
+           ms  <- groupByM (\(t0 , _ , _) (t1 , _ , _) -> not <$> step2MeasureChange t0 t1) (x:xs)
+           return (Section ts tpb
+                  $ map (Measure . map mkNote) ms)
+
+    -- translates a note
+    mkNote :: NoteEvent -> Note
+    mkNote (offset , dur , pitch) = Note offset dur pitch
     
-
-convertTrack :: TimeSig -> Midi.TimeDiv -> Midi.Track Midi.Ticks
-             -> Section
-convertTrack (TimeSig n d) (Midi.TicksPerSecond _ _) tr
-  = error "convertTrack: TicksPerSecond is unsupported"
-convertTrack (TimeSig n d) (Midi.TicksPerBeat bt) tr
-  = Section (TimeSig n d)
-  . map (Measure . map convert)
-  . groupBy sameMeasure
-  . getNoteEvents
-  $ tr
-  where
-    nbt = n * bt -- ticks per measure
-     
-    -- does two note events occur in the same measure?
-    sameMeasure :: NoteEvent -> NoteEvent -> Bool
-    sameMeasure (i0 , _ , _) (i1 , _ , _)
-      = (i0 `div` nbt) == (i1 `div` nbt)
-
-    -- TODO: we are not supporting note ties yet either....
-    convert :: NoteEvent -> Note
-    convert (_ , delta , k) = Note (durationInterp (fromIntegral delta / fromIntegral nbt))
-                                   (Pitch k)
-                                   False
-
--- |Main function here; given a midi file, returns a list of
---  sections. One for each track of the file.
---
---  TODO: We still do NOT support changes in time signatures
---        nor sectionswith multiple notes at the same time.
-fromMidi :: FilePath -> IO (Either String [Section])
-fromMidi file = either (Left . id) (Right . go)
-            <$> Midi.importFile file
-  where
-    go :: Midi.Midi -> [Section]
-    go midi = let tracks = Midi.tracks midi 
-                  tsig   = getTimeSig tracks
-               in map (convertTrack tsig (Midi.timeDiv midi)) tracks
+-- 
+fromMidi :: FilePath -> IO (Either String [Voice])
+fromMidi file
+  = do res <- Midi.importFile file
+       case res of
+         Left err -> return (Left err)
+         Right midi -> return $ 
+           if not (midiIsMultiTrack midi || midiIsTicksPerBeat midi)
+           then Left "Midi is not multi-track or not ticks-per-beat"
+           else Right . map Voice $ midiGetSections (midiAbsoluteOffsets midi)
