0ff66cc5477a71d779a7750310d296fe299287a7
Victor Miraldo <v.cacciarimiraldo@gmail.com>
2018-07-12 16:39:55 +0200

    Refactored example

--- src/Fragem/Metrics/BeatMetric.hs
+++ src/Fragem/Metrics/BeatMetric.hs
@@ -46,27 +46,3 @@ metricBeat ts tpb ms
                          , noteDelay $ head ns
                          )
 
-{-
-metricTest1 :: [Measure]
-metricTest1 = [ Measure [ Note i          whole       50
-                        , Note (i + wh)   eigth       47
-                        , Note (i + ww)   half        47
-                        , Note (i + wwh)  half        45
-                        , Note (i + wwwh) (half + wh) 50
-                        ]
-              , Measure [ Note (i + 4*w + wh) eigth 47
-                        , Note (i + 6*w)      half  47
-                        ]
-              ]
-  where
-    tpb = 60
-    i = 0
-    wh = whole + half
-    whole = tpb
-    w     = whole
-    ww    = 2*w
-    wwh   = w + wh
-    wwwh  = ww + wh
-    half  = whole `div` 2
-    eigth = half `div` 2
--}
