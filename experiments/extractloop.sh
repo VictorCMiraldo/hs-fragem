4531e985871a5457c006aa0637615aed67ab0159
irisyupingren <yuping.ren.iris@gmail.com>
2018-08-17 18:34:20 +0200

    fixed extract bash file

--- experiments/extractloop.sh
+++ experiments/extractloop.sh
@@ -1,11 +1,15 @@
 dir=hanon
-thre=1
+thre=0
 
 patfoldername=/home/irisren/Dropbox/111Projects/hs-fragem/experiments/extracted/
 filename="$dir-thre-$thre"
 
+
 for i in /home/irisren/Dropbox/111Projects/hs-fragem/dataset/$dir/* ; do
+    
     name=$(basename "$i")
+
+
     echo "processing file: $name"
     echo "creating prefix of pattern extracts: $filename"
     
@@ -16,8 +20,13 @@ for i in /home/irisren/Dropbox/111Projects/hs-fragem/dataset/$dir/* ; do
     mkdir "$dir/${name%%.*}"
     cd "$dir/${name%%.*}"
 
-    stack exec fragem -- threshold=$thre --export=$filename "/home/irisren/Dropbox/111Projects/hs-fragem/dataset/$dir/$name"
+    # logname="$name.txt"
+    # echo "creating log file $logname"
+    # touch $logname
+
+    stack exec fragem -- --threshold=$thre --export=$filename "/home/irisren/Dropbox/111Projects/hs-fragem/dataset/$dir/$name" | tee "$name.txt"
 
     cd ..
     cd ..
+
 done
