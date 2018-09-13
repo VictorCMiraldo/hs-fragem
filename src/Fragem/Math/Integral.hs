295b443c63a9640fc0389210d4592a2f883e90c5
irisyupingren <yuping.ren.iris@gmail.com>
2018-09-13 11:49:49 +0200

    solving xor area, ideas are here, syntax broken

--- src/Fragem/Math/Integral.hs
+++ src/Fragem/Math/Integral.hs
@@ -49,3 +49,23 @@ areaOfSegment points
             sqA    = deltaX * min py qy
             trA    = deltaX * abs (py - qy) / 2
          in sqA + trA + go q rest
+
+-- changing the input type: we need at least two notes at t1, and two notes at t2
+areaOfXORSegment :: [Point] -> [Point] -> Double
+areaOfSegment points1 points2
+  | length points1 < 2 || length points2 < 2 = 0
+  -- TODO, enforce this precond here instead of requiring it.
+  -- Iris: The syntax must be wrong here. But we go recursively we when have two lists of notes that line up nicely with each other 
+  | (p:ps) <- points1 (p2:ps2) <- points2 = go p ps p2 ps2
+  where
+    go :: [Point] -> [Point] -> Double
+    go _ [] = 0
+    -- Again the syntax is quite wrong. But to calculate the xor area given 4 points on a vertical grid is not hard :) (total height differeces * total time differences) * 0.5 = calculating two triangles at the same time
+    go p@(px , py) (q@(qx , qy) : rest) p2@(px2 , py2) (q2@(qx2 , qy2) : rest)
+      = let deltaX = qx - px
+            deltaY1 = qy - py
+            deltaY2 = qy2 - py2
+         in ((deltaY1 + deltaY2) * deltaX) * 0.5
+  
+  -- TODO, given a voice, find the corresponding notes in another voice: another function?
+  -- two different types for different voice? 
