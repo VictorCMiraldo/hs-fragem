0c2bca302fcaa17474ce64ec0320ef6a3c502cc6
Iris Yuping Ren <yuping.ren.iris@gmail.com>
2019-09-04 20:46:47 +0200

    added retrograde test case

--- test/Fragem/AnalisysSpec.hs
+++ test/Fragem/AnalisysSpec.hs
@@ -23,3 +23,8 @@ spec = do
     xit "Fails on inputs with length less than 2" $ property $
       notesMassLocal [] == Nothing .&&. notesMassLocal [Note undefined undefined undefined] == Nothing
   
+  
+    xit "Give the same mass when there's a retrograde" $ property $
+      (notesMassLocal [Note 0 40 120 , Note 40 40 60] `shouldBe` (Just 140.0))
+              .&&. (notesMassLocal [ Note 0 40 95 , Note 40 40 90 , Note 100 20 40]
+                    `shouldBe` (Just 170.6155281280883))
